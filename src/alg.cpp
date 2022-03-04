e1cae8af7de7aff334a44655d036822597e2c73f
Temidze <94217293+Temidze@users.noreply.github.com>
2022-03-04 16:50:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,43 @@
 
 
 double pown(double value, uint16_t n) {
-
+    double step = value;
+    for (uint16_t i=1; i < n; i++) {
+        step = step * value;
+    }
+    return(step);
 }
 
 uint64_t fact(uint16_t n) {
-
+    uint64_t s = 1;
+    for (uint16_t i = 1; i <= n; i++) {
+        s = s * i;
+    }
+    return(s);
 }
 
 double calcItem(double x, uint16_t n) {
-
+    return (pown(x, n) / fact(n));
 }
 
 double expn(double x, uint16_t count) {
-
+    double s = 0;
+    for (uint16_t i = 0; i <= count; i++) {
+        s += calcItem(x, i);
+    }
+    return(s);
 }
-
 double sinn(double x, uint16_t count) {
-
+    double s = 0;
+    for (uint16_t i = 1; i <= count; i++) {
+        s += pown(-1, i - 1) * calcItem(x, 2 * i - 1);
+    }
+    return(s);
 }
-
 double cosn(double x, uint16_t count) {
-
+    double c = 0;
+    for (uint16_t i = 1; i <= count; i++) {
+        c += pown(-1, i - 1) * calcItem(x, 2 * i - 2);
+    }
+    return(c);
 }
